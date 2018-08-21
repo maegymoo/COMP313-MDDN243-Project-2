3177c39ab8e376370fa7d465497f3c91f53aa712
Maegan Kennedy <11kema04@gmail.com>
2018-08-22 10:19:52 +1200

    got walking and jumping working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     private Rigidbody2D rb;
-    private float horizontal;
     private Animator animator;
-   
+
     [SerializeField]
-    private float speed;
+    private float movementSpeed;
 
     private bool facingRight;
+
+    [SerializeField]
+    private Transform[] groundPoints;
+
+    [SerializeField]
+    private float groundRaduis;
+
+    [SerializeField]
+    private LayerMask ground;
+
+    private bool isGrounded;
+
+    private bool jump;
+
+    private bool airControl;
+
+    [SerializeField]
+    private float jumpForce;
 
 	// Use this for initialization
 	void Start () {
@@ -19,21 +37,34 @@ public class Player : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void fixedUpdate () {
-        
-        horizontal = Input.GetAxis("Horizontal");
-        Movement(horizontal);
-        Animation();
-		
-	}
-    private void Movement(float horizontal) {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+    private void Update(){
+        CheckInput();
     }
 
-    private void Flip(float horizontal){
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        isGrounded = IsGrounded();
+        Move(horizontal);
+        Flip(horizontal);
+        ResetValues();
+		
+	}
+    private void Move(float horizontal) {
+        //rb.velocity = Vector2.left;
+        if (isGrounded || airControl) {
+            rb.velocity = new Vector2(horizontal * movementSpeed, rb.velocity.y);
+        }
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+        if (isGrounded && jump){
+            isGrounded = false;
+            rb.AddForce(new Vector2(0, jumpForce));
+
+        }
+    }
+
+    private void Flip(float horizontal) {
         if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
             facingRight = !facingRight;
 
@@ -43,7 +74,25 @@ public class Player : MonoBehaviour {
         }
     }
 
-    private void Animation() {
-        
+    private void CheckInput() {
+        jump |= Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private bool IsGrounded() {
+        if(rb.velocity.y <= 0) {
+            foreach (Transform pt in groundPoints){
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(pt.position, groundRaduis, ground);
+                foreach (Collider2D c in colliders){
+                    if (c.gameObject != gameObject){
+                        return true;
+                    }
+                    
+                }
+            }
+        }
+        return false;
+    }
+    private void ResetValues(){
+        jump = false;
     }
 }
