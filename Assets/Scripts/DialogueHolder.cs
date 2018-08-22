d50f876ecd60f95b80e3a58864c8c3e8d340b8c1
Guest User <Guest@wg401-f-01-06.mddn.vuw.ac.nz>
2018-08-22 13:55:52 +1200

    have background and ground tiles now, in process of adding in story. close #4 and #6

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager manager;
+
+	// Use this for initialization
+	void Start () {
+        manager = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D other){
+        if(other.gameObject.name == "Player"){
+            manager.ShowBox(dialogue);
+        }
+    }
+
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject dBox;
+
+    [SerializeField]
+    public Text dText;
+
+    [SerializeField]
+    public bool active;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(active&& Input.GetKeyDown(KeyCode.Return)){
+            dBox.SetActive(false);
+            active = false;
+        }
+	}
+
+    public void ShowBox(string dialogue){
+        active = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+}
